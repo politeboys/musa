546ec1b57e0dd7bc9377b8d3dfc8e162fb065a90
Your Name <you@example.com>
2021-01-17 03:31:36 +0600

    responsive

--- src/components/1-Sidebar/Sidebar.js
+++ src/components/1-Sidebar/Sidebar.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Sidebar.module.css'
 import Link from 'next/link'
-import { Grid } from '@material-ui/core';
+import { Grid, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Sidebar = () => {
+
+    if(true){
+        var openNav = () => {
+            const bar = document.getElementById('bar')
+            bar.classList.add('openNav')
+        }
+        var closeNav = () => {
+            const bar = document.getElementById('bar')
+            bar.classList.remove('openNav')
+        }}
+
+    // const [bar, setBar] = useState({openNav})
+    // if(openNav){
+    //     setBar(closeNav)
+    // }
+
+
     return (
             <nav className={styles.nav}>
                 <Grid container>
-                    <Grid className={styles.logo} item>
-                        <img style={{width: "100%"}} src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                    <Grid className={styles.mobileBar} item container justify="space-between" alignItems="center">
+                        <div className={styles.logo}><img style={{width: "100%"}} src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /></div>
+                        <IconButton onClick={openNav} className={styles.bar}>
+                            <MenuIcon/>
+                        </IconButton>
                     </Grid>
-                    <Grid className={styles.linkItems} item container direction="column">
-                        <Link href="/about"><a>About</a></Link>
-                        <Link href="/jobs"><a>Jobs</a></Link>
-                        <Link href="/contact"><a>Contact</a></Link>
-                        <Link href="/colophon"><a>Colophon</a></Link>
+                    <Grid id="bar" className={styles.linkItems} item container direction="column">
+                        <Link href="/about"><a onClick={closeNav}>About</a></Link>
+                        <Link href="/jobs"><a onClick={closeNav}>Jobs</a></Link>
+                        <Link href="/contact"><a onClick={closeNav}>Contact</a></Link>
+                        <Link href="/colophon"><a onClick={closeNav}>Colophon</a></Link>
                     </Grid>
                 </Grid>
             </nav>
